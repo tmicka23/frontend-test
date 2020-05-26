997401035727ab3c55bea8d237af1bfa5b977e23
tmicka23 <mickathp42@gmail.com>
2020-05-26 12:45:22 +0200

    test 3 not finished

--- test3/test3.js
+++ test3/test3.js
@@ -1,35 +1,47 @@
- /**
- * @TODO: 
- * Write a function that compute the sum of the numbers in a given 
+/**
+ * @TODO:
+ * Write a function that compute the sum of the numbers in a given
  * list using a for-loop.
  */
 function forLoopSum(list) {
-    return null;
+  let output = 0;
+  for (let i = 0; i < list.length; i++) {
+    output += list[i];
+  }
+  return output;
 }
 
- /**
- * @TODO: 
- * Write a function that compute the sum of the numbers in a given 
+/**
+ * @TODO:
+ * Write a function that compute the sum of the numbers in a given
  * list using a foreach-loop.
  */
 function forEachLoopSum(list) {
-    return null;
+  let output = 0;
+  list.forEach((li) => (output += li));
+  return output;
 }
 
 /**
-* @TODO: 
-* Write a function that compute the sum of the numbers in a given 
-* list using a while-loop.
-*/
+ * @TODO:
+ * Write a function that compute the sum of the numbers in a given
+ * list using a while-loop.
+ */
 function whileLoopSum(list) {
-   return null;
+  let output = 0;
+  let i = 0;
+  while (i <= list.length) {
+    output = list[i];
+    i++;
+  }
+  return output;
 }
 
 /**
-* @TODO: 
-* Write a function that compute the sum of the numbers in a given 
-* list using a recursive function.
-*/
+ * @TODO:
+ * Write a function that compute the sum of the numbers in a given
+ * list using a recursive function.
+ */
 function recursiveSum(list) {
-    return null;
+  return null;
 }
